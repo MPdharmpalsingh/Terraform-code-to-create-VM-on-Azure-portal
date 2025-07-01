b0b4aba4af22764cfaed9c01684b4346ee9498a5
Dharmpal <dps33261@gmail>
2025-07-01 19:14:44 +0530

    Reconnected project after .git loss

---
+++ provider.tf
@@ -0,0 +1,16 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "655adf33-92f7-4f42-a84f-47360e802df8"
+}
